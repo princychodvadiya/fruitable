f6a7abd7725240d6840ffc1a5ba27c402f0e8e7b
princychodvadiya <princychodvadiya721@gmail.com>
2024-03-21 21:47:34 +0530

    21-3-24

--- src/admin/container/Category/Category.js
+++ src/admin/container/Category/Category.js
@@ -8,12 +8,35 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import { object, string, number, date, InferType } from 'yup';
 import { useFormik } from 'formik';
-import { hasFormSubmit } from '@testing-library/user-event/dist/utils';
-import { json } from 'react-router-dom';
 import { useState } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import DeleteIcon from '@mui/icons-material/Delete';
 import { useEffect } from 'react';
 
 export default function FormDialog() {
+
+    const columns = [
+        { field: 'category', headerName: 'Name' },
+        { field: 'description', headerName: 'Description' },
+        {
+            field: 'delete', headerName: 'Delete',
+            renderCell: (params) => (
+                // <Button
+                <DeleteIcon onClick={() => handleDelete(params.row.id)} />
+                // >
+                // </Button>
+            ),
+        }
+    ];
+
+    const handleDelete = (id) => {
+        // console.log(id);
+        let localData = JSON.parse(localStorage.getItem('category'));
+        localData = localData.filter((v) => v.id !== id);
+        // console.log(localData);
+        localStorage.setItem('category', JSON.stringify(localData))
+        getdata()
+    }
 
     let CategorySchema = object({
         category: string().required("please enter name"),
@@ -31,21 +54,22 @@ export default function FormDialog() {
             setdata(localData)
         }
         console.log(localData);
-
-
     }
 
     useEffect(() => {
         getdata()
     }, [])
+
     const formik = useFormik({
         initialValues: {
             category: '',
             description: '',
         },
         validationSchema: CategorySchema,
-        onSubmit: (values) => {
+        onSubmit: (values, { resetForm }) => {
+            resetForm()
             handleAdd(values)
+            handleClose()
         },
     })
 
@@ -63,7 +87,7 @@ export default function FormDialog() {
 
     const handleAdd = (data) => {
         console.log(data);
-        const rNo = Math.floor(Math.rendom * 1000);
+        const rNo = Math.floor(Math.random() * 1000)
 
         let localData = JSON.parse(localStorage.getItem("category"));
         if (localData) {
@@ -71,60 +95,75 @@ export default function FormDialog() {
             localStorage.setItem('category', JSON.stringify(localData))
 
         } else {
-            localStorage.setItem('category', JSON.stringify({ ...data, id: rNo }));
+            localStorage.setItem('category', JSON.stringify([{ ...data, id: rNo }]));
         }
+        getdata()
     }
 
     return (
-        <React.Fragment>
-            <Button variant="outlined" onClick={handleClickOpen}>
-                Add Category
-            </Button>
-            <Dialog
-                open={open}
-                onClose={handleClose}
+        <div>
+            <React.Fragment>
+                <Button variant="outlined" onClick={handleClickOpen}>
+                    Add Category
+                </Button>
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
 
-            >
-                <DialogTitle>Category</DialogTitle>
-                <form onSubmit={handleSubmit}>
-                    <DialogContent>
-                        <TextField
-                            margin="dense"
-                            id="name"
-                            name="category"
-                            label="category"
-                            type="text"
-                            fullWidth
-                            variant="standard"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.category}
-                            error={errors.category && errors.category ? true : false}
-                            helperText={errors.category && touched.category ? errors.category : ''}
-                        />
-                        <TextField
-                            margin="dense"
-                            id="name"
-                            name="description"
-                            label="description"
-                            type="text"
-                            fullWidth
-                            variant="standard"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.description}
-                            error={errors.description && errors.description ? true : false}
-                            helperText={errors.description && touched.description ? errors.description : ''}
-                        />
-                        <DialogActions>
-                            <Button onClick={handleClose}>Cancel</Button>
-                            <Button type="submit">ADD</Button>
-                        </DialogActions>
-                    </DialogContent>
+                >
+                    <DialogTitle>Category</DialogTitle>
+                    <form onSubmit={handleSubmit}>
+                        <DialogContent>
+                            <TextField
+                                margin="dense"
+                                id="name"
+                                name="category"
+                                label="category"
+                                type="text"
+                                fullWidth
+                                variant="standard"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.category}
+                                error={errors.category && errors.category ? true : false}
+                                helperText={errors.category && touched.category ? errors.category : ''}
+                            />
+                            <TextField
+                                margin="dense"
+                                id="name"
+                                name="description"
+                                label="description"
+                                type="text"
+                                fullWidth
+                                variant="standard"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.description}
+                                error={errors.description && errors.description ? true : false}
+                                helperText={errors.description && touched.description ? errors.description : ''}
+                            />
+                            <DialogActions>
+                                <Button onClick={handleClose}>Cancel</Button>
+                                <Button type="submit">ADD</Button>
+                            </DialogActions>
+                        </DialogContent>
+                    </form>
+                </Dialog>
+            </React.Fragment>
 
-                </form>
-
-            </Dialog>
-        </React.Fragment>
+            <div style={{ height: 400, width: '100%' }}>
+                <DataGrid
+                    rows={data}
+                    columns={columns}
+                    initialState={{
+                        pagination: {
+                            paginationModel: { page: 0, pageSize: 5 },
+                        },
+                    }}
+                    pageSizeOptions={[5, 10]}
+                    checkboxSelection
+                />
+            </div>
+        </div>
     );
 }
